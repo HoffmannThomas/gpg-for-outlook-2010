23e226ac877506efc07994d4cde54da0d55d9ff9
thomashoffmann.exe <thomashoffmann.exe@googlemail.com>
2012-04-11 13:25:46 +0200

    * ensure plain text mail body format

--- OutlookGpg2010/Ribbons/GpgRibbonCompose.cs
+++ OutlookGpg2010/Ribbons/GpgRibbonCompose.cs
@@ -12,6 +12,8 @@ namespace OutlookGpg2010
 
             this.signMailCheck.Checked = Properties.userSettings.Default.AlwaysSign;
             this.encryptMailCheck.Checked = Properties.userSettings.Default.AlwaysEncrypt;
+
+            if (this.encryptMailCheck.Checked) { this.setMailBodyFormatPlain(); }
         }
 
         private void Application_ItemSend(object Item, ref bool Cancel)
@@ -30,7 +32,7 @@ namespace OutlookGpg2010
 
                         if (this.signMailCheck.Checked && !this.encryptMailCheck.Checked) { mail.Body = ((new GPG4OutlookLib.Methods.Sign(myEmailAddress)).execute(mail.Body)).message; }
                         if (!this.signMailCheck.Checked && this.encryptMailCheck.Checked) { mail.Body = ((new GPG4OutlookLib.Methods.Encrypt(mail.Recipients)).execute(mail.Body)).message; }
-                        if (this.signMailCheck.Checked && this.encryptMailCheck.Checked) { mail.Body = ((new GPG4OutlookLib.Methods.SignAndEncrypt(mail.Recipients,myEmailAddress)).execute(mail.Body)).message; }
+                        if (this.signMailCheck.Checked && this.encryptMailCheck.Checked) { mail.Body = ((new GPG4OutlookLib.Methods.SignAndEncrypt(mail.Recipients, myEmailAddress)).execute(mail.Body)).message; }
                     }
                     catch (System.Exception ex)
                     {
@@ -43,20 +45,24 @@ namespace OutlookGpg2010
 
         private void signMailCheck_Click(object sender, RibbonControlEventArgs e)
         {
-
         }
 
         private void encryptMailCheck_Click(object sender, RibbonControlEventArgs e)
         {
-           Inspector inspector = Globals.ThisAddIn.Application.ActiveInspector();
-
-           MailItem mail = (MailItem)inspector.CurrentItem;
-           mail.BodyFormat = OlBodyFormat.olFormatPlain;
+            this.setMailBodyFormatPlain();
         }
 
         private void settingsButton_Click(object sender, RibbonControlEventArgs e)
         {
             new Tools.SettingsForm().Show();
+        }
+
+        private void setMailBodyFormatPlain()
+        {
+            Inspector inspector = Globals.ThisAddIn.Application.ActiveInspector();
+
+            MailItem mail = (MailItem)inspector.CurrentItem;
+            mail.BodyFormat = OlBodyFormat.olFormatPlain;
         }
     }
 }
